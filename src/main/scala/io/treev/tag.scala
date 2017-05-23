a4bbc9ad2c79809501dbf92fdc9389cb18e5e1bf
Alexander Semenov <tvaroh@icloud.com>
2017-05-23 17:00:36 +0300

    Add tagging with TaggedType to container and "and" tagging extensions.

--- src/main/scala/io/treev/tag.scala
+++ src/main/scala/io/treev/tag.scala
@@ -11,56 +11,6 @@ object tag {
   type Tagged[+T, +U] = T with Tag[U]
   type @@[+T, +U] = Tagged[T, U]
 
-  /** Function-first-style tagging API.
-    * @tparam U type to tag with
-    * @return `Tagger` instance that can be used for tagging */
-  def apply[T, U](t: T): T @@ U = t.asInstanceOf[T @@ U]
-
-  implicit class TaggingExtensions[T](val t: T) extends AnyVal {
-    /** Tag with type `U`.
-      * @tparam U type to tag with
-      * @return value tagged with `U` */
-    def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
-
-    /** Synonym operator for `taggedWith`. */
-    def @@[U]: T @@ U = taggedWith[U]
-
-    /** Type tag using `TaggedType` instance.
-      * Allows for `String @@ MyTaggedType` syntax. */
-    def taggedWith[U](taggedType: TaggedType[U]): T @@ taggedType.Tag = taggedWith[taggedType.Tag]
-
-    /** Synonym operator for `taggedWith`. */
-    def @@[U](taggedType: TaggedType[U]): T @@ taggedType.Tag = taggedWith[taggedType.Tag]
-  }
-  implicit class AndTaggingExtensions[T, U](val t: T @@ U) extends AnyVal {
-    /** Tag tagged value with type `V`.
-      * @tparam V type to tag with
-      * @return value tagged with both `U` and `V` */
-    def andTaggedWith[V]: T @@ (U with V) = t.asInstanceOf[T @@ (U with V)]
-
-    /** Synonym operator for `andTaggedWith`. */
-    def +@[V]: T @@ (U with V) = andTaggedWith[V]
-  }
-
-  implicit class TaggingExtensionsF[F[_], T](val ft: F[T]) extends AnyVal {
-    /** Tag intra-container values with type `U`.
-      * @tparam U type to tag with
-      * @return container with nested values tagged with `U` */
-    def taggedWithF[U]: F[T @@ U] = ft.asInstanceOf[F[T @@ U]]
-
-    /** Synonym operator for `taggedWithF`. */
-    def @@@[U]: F[T @@ U] = taggedWithF[U]
-  }
-  implicit class AndTaggingExtensionsF[F[_], T, U](val ft: F[T @@ U]) extends AnyVal {
-    /** Tag tagged intra-container values with type `U`.
-      * @tparam V type to tag with
-      * @return container with nested values tagged with both `U` and `V` */
-    def andTaggedWithF[V]: F[T @@ (U with V)] = ft.asInstanceOf[F[T @@ (U with V)]]
-
-    /** Synonym operator for `andTaggedWithF`. */
-    def +@@[V]: F[T @@ (U with V)] = andTaggedWithF[V]
-  }
-
   /** Base tagged type trait.
     * @tparam R raw value type */
   trait TaggedType[R] {
@@ -74,6 +24,85 @@ object tag {
 
     /** Create tagged value from raw value. */
     def apply(raw: Raw): Type = raw.@@[Tag]
+  }
+
+  /** Function-first-style tagging API.
+    * @tparam U type to tag with
+    * @return `Tagger` instance that can be used for tagging */
+  def apply[T, U](t: T): T @@ U = t.asInstanceOf[T @@ U]
+
+  implicit class TaggingExtensions[T](val t: T) extends AnyVal {
+
+    /** Tag with type `U`.
+      * @tparam U type to tag with
+      * @return value tagged with `U` */
+    def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
+    /** Synonym operator for `taggedWith`. */
+    def @@[U]: T @@ U = taggedWith[U]
+
+    /** Tag using `TaggedType` instance.
+      * Allows for `String @@ MyTaggedType` syntax.
+      * @param taggedType tagged type to type-tag with using its inner `Tag` field
+      * @return value tagged with `taggedType.Tag` */
+    def taggedWith(taggedType: TaggedType[_]): T @@ taggedType.Tag = taggedWith[taggedType.Tag]
+    /** Synonym operator for `taggedWith`. */
+    def @@(taggedType: TaggedType[_]): T @@ taggedType.Tag = taggedWith[taggedType.Tag]
+
+  }
+  implicit class AndTaggingExtensions[T, U](val t: T @@ U) extends AnyVal {
+
+    /** Tag tagged value with type `V`.
+      * @tparam V type to tag with
+      * @return value tagged with both `U` and `V` */
+    def andTaggedWith[V]: T @@ (U with V) = t.asInstanceOf[T @@ (U with V)]
+    /** Synonym operator for `andTaggedWith`. */
+    def +@[V]: T @@ (U with V) = andTaggedWith[V]
+
+    /** Tag tagged value using `TaggedType` instance.
+      * Allows for `String +@ MyTaggedType` syntax.
+      * @param taggedType tagged type to type-tag with using its inner `Tag` field
+      * @return value tagged with both `U` and `taggedType.Tag` */
+    def andTaggedWith(taggedType: TaggedType[_]): T @@ (U with taggedType.Tag) = andTaggedWith[taggedType.Tag]
+    /** Synonym operator for `andTaggedWith`. */
+    def +@(taggedType: TaggedType[_]): T @@ (U with taggedType.Tag) = andTaggedWith[taggedType.Tag]
+
+  }
+
+  implicit class TaggingExtensionsF[F[_], T](val ft: F[T]) extends AnyVal {
+
+    /** Tag intra-container values with type `U`.
+      * @tparam U type to tag with
+      * @return container with nested values tagged with `U` */
+    def taggedWithF[U]: F[T @@ U] = ft.asInstanceOf[F[T @@ U]]
+    /** Synonym operator for `taggedWithF`. */
+    def @@@[U]: F[T @@ U] = taggedWithF[U]
+
+    /** Tag intra-container values using `TaggedType` instance.
+      * Allows for `List(...) @@ MyTaggedType` syntax.
+      * @param taggedType tagged type to type-tag with using its inner `Tag` field
+      * @return container with nested values tagged with `taggedType.Tag` */
+    def taggedWithF(taggedType: TaggedType[_]): F[T @@ taggedType.Tag] = taggedWithF[taggedType.Tag]
+    /** Synonym operator for `taggedWith`. */
+    def @@@(taggedType: TaggedType[_]): F[T @@ taggedType.Tag] = taggedWithF[taggedType.Tag]
+
+  }
+  implicit class AndTaggingExtensionsF[F[_], T, U](val ft: F[T @@ U]) extends AnyVal {
+
+    /** Tag tagged intra-container values with type `U`.
+      * @tparam V type to tag with
+      * @return container with nested values tagged with both `U` and `V` */
+    def andTaggedWithF[V]: F[T @@ (U with V)] = ft.asInstanceOf[F[T @@ (U with V)]]
+    /** Synonym operator for `andTaggedWithF`. */
+    def +@@[V]: F[T @@ (U with V)] = andTaggedWithF[V]
+
+    /** Tag tagged intra-container values using `TaggedType` instance.
+      * Allows for `List(...) +@@ MyTaggedType` syntax.
+      * @param taggedType tagged type to type-tag with using its inner `Tag` field
+      * @return container with nested values tagged with both `U` and `taggedType.Tag` */
+    def andTaggedWithF(taggedType: TaggedType[_]): F[T @@ (U with taggedType.Tag)] = andTaggedWithF[taggedType.Tag]
+    /** Synonym operator for `andTaggedWithF`. */
+    def +@@(taggedType: TaggedType[_]): F[T @@ (U with taggedType.Tag)] = andTaggedWithF[taggedType.Tag]
+
   }
 
 }
